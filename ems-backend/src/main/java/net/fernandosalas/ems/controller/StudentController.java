e124f2cba9100ba351142ec4c13e024f851be540
Fernando Salas <luisfernandosalasg@gmail.com>
2023-08-10 02:06:20 -0500

    Create Student Controller and createStudent REST API

---
+++ ems-backend/src/main/java/net/fernandosalas/ems/controller/StudentController.java
@@ -0,0 +1,24 @@
+package net.fernandosalas.ems.controller;
+import lombok.AllArgsConstructor;
+import net.fernandosalas.ems.dto.StudentDto;
+import net.fernandosalas.ems.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/students")
+@AllArgsConstructor
+public class StudentController {
+    @Autowired
+    private StudentService studentService;
+    @PostMapping
+    public ResponseEntity<StudentDto> createStudent(@RequestBody StudentDto studentDto) {
+       StudentDto savedStudent =  studentService.createStudent(studentDto);
+       return new ResponseEntity<>(savedStudent, HttpStatus.CREATED);
+    }
+}
