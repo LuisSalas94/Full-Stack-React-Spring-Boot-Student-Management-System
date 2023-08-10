f9b08f05ac5c8a20af0a0a4f282b51bb19690c15
Fernando Salas <luisfernandosalasg@gmail.com>
2023-08-10 06:23:51 -0500

    Create Department Entity

---
+++ ems-backend/src/main/java/net/fernandosalas/ems/entity/Department.java
@@ -0,0 +1,23 @@
+package net.fernandosalas.ems.entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "departments")
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "department_name")
+    private String departmentName;
+
+    @Column(name = "department_description")
+    private String departmentDescription;
+}
