a25fffe4cac872ff8218279db85bf0fafe6e3f4b
Fernando Salas <luisfernandosalasg@gmail.com>
2023-08-10 01:41:53 -0500

    Create Student Entity

---
+++ ems-backend/src/main/java/net/fernandosalas/ems/entity/Student.java
@@ -0,0 +1,26 @@
+package net.fernandosalas.ems.entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "students")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+}
